605b04baa1d13d5a4e7ab7493ee3ed4d52caf73c
pkantyka <59740730+pkantyka@users.noreply.github.com>
2022-04-07 15:42:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,9 @@ import streamlit as st
 
 import cx_Oracle
 
-#connection = cx_Oracle.connect(
-#    user="demopython",
-#    password="XXXXX",
-#    dsn="localhost/xepdb1")
+connection = cx_Oracle.connect(
+    user="demopython",
+    password="XXXXX",
+    dsn="localhost/xepdb1")
 
 st.write("Successfully run with CX Oracle")
