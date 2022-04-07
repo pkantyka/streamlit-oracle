7cad8c20075a419c1a65bc3e880553346b2c65eb
pkantyka <59740730+pkantyka@users.noreply.github.com>
2022-04-07 15:42:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
 import streamlit as st
-x = st.slider('x')  # 👈 this is a widget
-st.write(x, 'squared is', x * x)
+#x = st.slider('x')  # 👈 this is a widget
+#st.write(x, 'squared is', x * x)
+
+import cx_Oracle
+
+#connection = cx_Oracle.connect(
+#    user="demopython",
+#    password="XXXXX",
+#    dsn="localhost/xepdb1")
+
+st.write("Successfully run with CX Oracle")
