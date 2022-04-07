bbb41d18ba38e4b086b801170880bbc337eca4df
pkantyka <59740730+pkantyka@users.noreply.github.com>
2022-04-07 15:01:54 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,8 @@
-#
+import cx_Oracle
+
+connection = cx_Oracle.connect(
+    user="demopython",
+    password="XXXXX",
+    dsn="localhost/xepdb1")
+
+sl.write("Successfully connected to Oracle Database")
